83df6227f3eb8ea43c901e78386fe52103f92c7b
Adam Spiers <blockchain@adamspiers.org>
2022-08-21 16:41:35 +0100

    Add useAppDispatch
    Add a typed version of useDispatch which infers the Dispatch type from
    the store, as recommended here:
    
    https://redux-toolkit.js.org/usage/usage-with-typescript#getting-the-dispatch-type

--- packages/example-next/redux/store.ts
+++ packages/example-next/redux/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch } from 'react-redux'
 import counterReducer from './slices/counter'
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     counter: counterReducer
   },
@@ -11,3 +12,7 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+
+export default store
